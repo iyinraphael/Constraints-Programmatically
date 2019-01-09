26f15ae8477098e1f681c0ce13ccf79ed1f9bff5
iyinraphael <iyinraphael@gmail.com>
2019-01-08 22:21:15 -0800

    used Autolayout to create new subview

--- Constraint Programmatically/ViewController.swift
+++ Constraint Programmatically/ViewController.swift
@@ -12,7 +12,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let greenSquare = UIView()
+        greenSquare.translatesAutoresizingMaskIntoConstraints = false
+        greenSquare.backgroundColor = .green
+        view.addSubview(greenSquare)
+        
+        let widthConstraint = NSLayoutConstraint(item: greenSquare,
+                                                 attribute: .width,
+                                                 relatedBy: .equal,
+                                                 toItem: nil,
+                                                 attribute: .notAnAttribute,
+                                                 multiplier: 1.0,
+                                                 constant: 200.0)
+        
+        let heightConstraint = NSLayoutConstraint(item: greenSquare,
+                                                  attribute: .height,
+                                                  relatedBy: .equal,
+                                                  toItem: greenSquare,
+                                                  attribute: .width,
+                                                  multiplier: 1.0,
+                                                  constant: 0.0)
+        
+        let centerYContraint = NSLayoutConstraint(item: greenSquare,
+                                                  attribute: .centerY,
+                                                  relatedBy: .equal,
+                                                  toItem: view,
+                                                  attribute: .centerY,
+                                                  multiplier: 1.0,
+                                                  constant: 0.0)
+        
+        let centerXContraint = NSLayoutConstraint(item: greenSquare,
+                                                  attribute: .centerX,
+                                                  relatedBy: .equal,
+                                                  toItem: view,
+                                                  attribute: .centerX,
+                                                  multiplier: 1.0,
+                                                  constant: 0.0)
+        
+        NSLayoutConstraint.activate([heightConstraint, widthConstraint, centerXContraint, centerYContraint])
     }
 
 
